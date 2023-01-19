860a9fe4990ab47c077369e61a59b6228c6a7ad5
Ermol <ermol-club@yandex.ru>
2023-01-19 06:44:29 +0300

    Задача 2 выполнена

---
+++ Desktop/GB/С#/Dotnet/HomeWork1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Первое число");
+int a  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b < a)
+{
+    Console.WriteLine($"{a} - максимальное число");
+}
+else 
+{
+    Console.WriteLine($"{b} - максимальное число");
+}
+
