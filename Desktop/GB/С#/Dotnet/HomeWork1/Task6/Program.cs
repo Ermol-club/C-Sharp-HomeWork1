9ab314bc423f95ecb7a0805c1bb54f020ff94df4
Ermol <ermol-club@yandex.ru>
2023-01-20 21:28:59 +0300

    Программа 6 работает

---
+++ Desktop/GB/С#/Dotnet/HomeWork1/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача на определение, является ли введенное число чётным (делится ли оно на два без остатка) или нет.
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int a  = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} - число четное");
+} 
+else
+{
+Console.WriteLine($"{a} - число нечетное");
+}
