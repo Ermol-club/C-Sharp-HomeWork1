c34c911cbb2309b784bf8d401e990f6e2c504aa5
Ermol <ermol-club@yandex.ru>
2023-01-20 21:27:04 +0300

    Программа работает

---
+++ Desktop/GB/С#/Dotnet/HomeWork1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача на нахлждение максимального из трех чисел
+
+Console.Clear();
+Console.WriteLine("Введите Первое число");
+int a  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c  = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное число из трех = ");
+Console.WriteLine(max);
