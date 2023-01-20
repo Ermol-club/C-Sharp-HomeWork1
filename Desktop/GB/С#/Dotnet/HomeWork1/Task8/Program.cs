4796f18b9570fa0d9b8147c15c00621c6514bfb0
Ermol <ermol-club@yandex.ru>
2023-01-20 21:29:56 +0300

    Программа 8 работает

---
+++ Desktop/GB/С#/Dotnet/HomeWork1/Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача на нахлждение всех четных чисел до N
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n  = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count < n)
+{
+    Console.Write($"{count},");
+    count = count + 2;
+}
