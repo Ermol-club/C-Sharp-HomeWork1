5eed6f37965a7e0a8012adde762be9255612c817
Ermol <ermol-club@yandex.ru>
2023-01-25 04:27:35 +0300

    Хотел написать функцию, но не смог)

---
+++ Desktop/GB/С#/HomeWork2/Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая выводит третью цифру заданного числа, или сообщает что третьей цифры нет
+
+Console.Clear();
+System.Console.WriteLine("Введите число --> ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+def f3(x);
+    if (x >= 999)
+    {
+        while (x >= 999)
+        x = x / 10;
+        return (x % 1 * 10);
+    }
+    else
+    {
+        return;
+    }
+
+
+
+
+
+
+
+int Max(int a, int b)  //Функция
+{
+    if (a > b) return a;
+    else return b;
+}
