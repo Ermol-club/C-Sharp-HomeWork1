44c027dca1921b5c07f27c8e14df8cd722f90c70
Ermol <ermol-club@yandex.ru>
2023-01-25 03:10:48 +0300

    Task15

---
+++ Desktop/GB/С#/HomeWork2/Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿// Программа, определяющая по введенному числу день недели, выхлдной или нет
+
+System.Console.Write("Введите номер дня --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a >= 1)&&(a <= 5))
+{
+    Console.WriteLine("Это будний день");
+}
+
+if ((a >= 6)&&(a <= 7))
+{
+    Console.WriteLine("Это выходной день");
+}
+if ((a == 0)&&(a >=8 ))
+{
+    Console.WriteLine("Введите номер дня от 1 до 7");
+}
+
+
+
+
+
+//{
+//    System.Console.Write("--> ");
+//    number = Convert.ToInt32(Console.ReadLine());
+//    return number;
+//}
+//
+//double num = newNumber();
+//if ((num % 7 == 0)&&(num % 23 == 0))              //Так мы проверяем делится ли число без остатка на 7 и 23
+//{
+//    System.Console.WriteLine(num + " кратно 7 и 23");
+//}
+//else 
+//{
+//   System.Console.WriteLine(num + " не кратно 7 и 23");
+//} 
