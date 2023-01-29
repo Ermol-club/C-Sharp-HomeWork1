cfae37547cfbe15d190bf16b44087b2c83998ee0
Ermol <ermol-club@yandex.ru>
2023-01-29 21:20:45 +0300

    Task23

---
+++ Desktop/GB/С#/HomeWork3/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Gрограммf, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Enter your number -->");
+int n = Convert.ToInt32(System.Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    System.Console.Write(Math.Pow(i,3) + ", ");
+    i++;
+}
+
+
