3e1d53d24dd7b46daf7e599694f90fa3895d2aa5
Ermol <ermol-club@yandex.ru>
2023-01-29 21:17:50 +0300

    Task19

---
+++ Desktop/GB/С#/HomeWork3/Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+System.Console.WriteLine("Введите пятизначное число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = a /10000;                                         // Определение первой цифры числа
+int lastNumber = a % 10;                                            // Определение последней цифры числа
+int secondNumber = (a / 1000) % 10;
+int forthNumber = (a % 100) / 10;
+
+if ((firstNumber == lastNumber) && (secondNumber == forthNumber))
+{
+    Console.WriteLine("O, Да!");
+} 
+
+else System.Console.WriteLine("Oh? now!");
