7714dc1cf6259ceb65d98508fc555bb952db489b
Ermol <ermol-club@yandex.ru>
2023-01-30 23:35:20 +0300

    Task 25

---
+++ Desktop/GB/С#/HomeWork4/Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Enter 1st number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2nd number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int summ = 1;
+
+for ( int i = 0; i < b; i++ )
+    {
+        summ = summ * a;
+    }
+Console.WriteLine("Result = " + summ + ".");
