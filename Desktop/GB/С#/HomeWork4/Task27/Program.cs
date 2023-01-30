aaf9bbe7fff9624f5b7721349d2205976e0d59c4
Ermol <ermol-club@yandex.ru>
2023-01-30 23:34:46 +0300

    Task 27

---
+++ Desktop/GB/С#/HomeWork4/Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.Write("Enter your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while ( a > 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    
+Console.WriteLine("Summ = " + sum + ".");
