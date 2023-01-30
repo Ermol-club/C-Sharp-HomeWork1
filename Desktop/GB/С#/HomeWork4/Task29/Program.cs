a14d9c83d89471f0bc9a41e6f485313aec75d19e
Ermol <ermol-club@yandex.ru>
2023-01-30 23:33:45 +0300

    Task 29

---
+++ Desktop/GB/С#/HomeWork4/Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] myArr = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < myArr.Length; i++)
+{
+    myArr[i] = rnd.Next(10);
+}
+
+for (int i = 0; i < myArr.Length; i++)
+{
+        System.Console.Write("[" + myArr[i] + "] ");
+}
+
+
+
