552d96cb4a9af9f250eabc9af20cf697ef9b415e
Ermol <ermol-club@yandex.ru>
2023-01-31 22:27:59 +0300

    Task38

---
+++ Desktop/GB/С#/HomeWork5/Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] arr = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(1000);
+    System.Console.Write(arr[i] + " ");
+}
+
+int maxNumber = 0;
+int minNumber = 999;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > maxNumber)
+    {
+        maxNumber = arr[i];
+    }
+
+    if(arr[i] < minNumber)
+    {
+        minNumber = arr[i];
+    }
+}
+System.Console.WriteLine("\n" + "Максимальный элемент: " + maxNumber + "\n" + "Минимальный элемент: " + minNumber);
+
+
+
+
