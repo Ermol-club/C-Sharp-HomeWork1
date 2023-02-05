80807db046a163f04b61b70ac1b31818dd6ec088
Ermol <ermol-club@yandex.ru>
2023-02-05 04:07:54 +0300

    Task41

--- Desktop/GB/С#/HomeWork6/Task41/Program.cs
+++ Desktop/GB/С#/HomeWork6/Task41/Program.cs
@@ -1,17 +1,15 @@
 ﻿//41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.WriteLine("Enter your nubbers  ---> ");
-string str = Console.ReadLine();
+Console.WriteLine("Enter your nubbers через запятую ---> ");
+//string str = Console.ReadLine();
 
-char[] separ = new char[] {' ', '/'};
-string[] array = str.Split(separ,StringSplitOptions.RemoveEmptyEntries);
 
-int[] x = Array.ConvertAll(array,int.Parse);
+int[] m = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
 Console.WriteLine("{"+string.Join(",",x)+"}");
 
-int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (x[i]>0) count++;
-}
-Console.WriteLine("Elements > 0: " + count);
+int count = m.Count(x => x > 0);
+//for (int i = 0; i < array.Length; i++)
+//{
+    //if (x[i]>0) count++;
+//}
+Console.WriteLine($"Elements > 0: {count}");
