0b03b5963821ff64b1fdf0e867ed96637e83cb31
Ermol <ermol-club@yandex.ru>
2023-02-12 07:54:42 +0300

    Task64

---
+++ Desktop/GB/С#/HomeWork9/Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте М и Н. Программа выводит все числа от М до Н.
+
+int n = 3;;
+int m = 10;;
+NumRec(n,m);
+
+void NumRec(int n, int m)
+{
+    if (m<n) return;
+    else System.Console.Write($"{n},");
+    NumRec(n+1,m);
+}
