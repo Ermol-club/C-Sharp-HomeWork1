a613df608554e751b631daf12d5414113fc4bcf3
Ermol <ermol-club@yandex.ru>
2023-02-12 07:56:46 +0300

    Task68

---
+++ Desktop/GB/С#/HomeWork9/Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа вычесления финкции Аккермана. Даны два неотрицательных числа.
+
+int m = 1;
+int n = 2;
+int Akker(int m, int n)
+{
+        if (n == 0)
+        return m + 1;
+    else
+        if (n == 0)
+        return Akker(m - 1, 1);
+    else
+        return Akker(m - 1, Akker(m, n - 1));
+}
+Console.WriteLine(Akker(m, n));
+
