89fe0c9a1bed7ff9b1dac1ce2515128a7a1ef36b
Ermol <ermol-club@yandex.ru>
2023-02-12 07:56:02 +0300

    Task66

---
+++ Desktop/GB/С#/HomeWork9/Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте М и Н. Найдите сумму натуральных элементов от М до Н.
+
+int m = 7;
+int n = 15;
+int Sum(int m, int n)
+{
+    if (n == m) return 0;
+    else
+    {
+        return m + Sum(m+1, n);
+    }
+}
+System.Console.WriteLine(Sum(m,n));
+
+
+
