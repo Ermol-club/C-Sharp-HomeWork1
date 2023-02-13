bd7994e4abf1af94104e858ee4cba49808526508
Ermol <ermol-club@yandex.ru>
2023-02-13 20:07:53 +0300

    Task58

---
+++ Desktop/GB/С#/HomeWork8/Task58/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int[,] firstMartrix = new int[3, 3];
+FullArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[3, 3];
+FullArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[3,3];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        int sum = 0;      
+        sum = firstMartrix[i,j] * secomdMartrix[i,j];
+        resultMatrix[i,j] = sum;
+    }
+  }
+}
+WriteArray(resultMatrix);
+
+void FullArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++) 
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,10);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
