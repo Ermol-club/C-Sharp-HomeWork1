a8106c393902e341850547f77b87f7cb06a75323
Ermol <ermol-club@yandex.ru>
2023-03-06 18:37:11 +0300

    Задали массив данных типа стринг и разделили его по запятой

---
+++ Desktop/GB/3 Знакомство с С#/Final Task C#/Program.cs
@@ -0,0 +1,9 @@
+﻿// Контрольное задание C#
+
+string [] arrayFirst =  {"Hello", "2", "World", ":)"};
+Console.WriteLine($"[{string.Join(", ", arrayFirst)}]");
+
+string [] array.Second = FindStringBelow4(arrayFirst);
+Console.WriteLine($"[{atring.Join(", ", arraySecond)}]");
+
+string 
