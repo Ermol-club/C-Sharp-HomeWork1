96c11bfa80425c1f7d77a2eba87020c97349b846
Ermol <ermol-club@yandex.ru>
2023-03-06 18:52:10 +0300

    2 commit

--- Desktop/GB/3 Знакомство с С#/Final Task C#/Program.cs
+++ Desktop/GB/3 Знакомство с С#/Final Task C#/Program.cs
@@ -3,7 +3,20 @@
 string [] arrayFirst =  {"Hello", "2", "World", ":)"};
 Console.WriteLine($"[{string.Join(", ", arrayFirst)}]");
 
-string [] array.Second = FindStringBelow4(arrayFirst);
-Console.WriteLine($"[{atring.Join(", ", arraySecond)}]");
+string [] arraySecond = FindStringBelow4(arrayFirst);
+Console.WriteLine($"[{string.Join(", ", arraySecond)}]");
 
-string 
+string [] FindStringBelow4(string[] arrayStart)
+{
+int n=0,j=0;
+foreach (string item in arrayStart)
+{
+   if (item.Length<4) n++;
+}
+string []arrayResult= new string [n];
+foreach (string item in arrayStart)
+{
+   if (item.Length<4) {arrayResult[j]=item; j++;}
+}
+return arrayResult;
+}
